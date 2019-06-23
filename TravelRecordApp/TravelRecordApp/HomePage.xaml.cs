3d50a6d58ed9668be5ec6a7b44690f76201571e7
James Bouy <jbouy@turner-industries.com>
2019-06-23 00:41:53 -0500

    s4: XAML & Navigation - The travel record app

---
+++ TravelRecordApp/TravelRecordApp/HomePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewTravelPage());
+        }
+    }
+}
---
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_OnClicked(object sender, EventArgs e)
+        {
+            var isEmailEmpty = string.IsNullOrEmpty(EmailEntry.Text);
+            var isPasswordEmpty = string.IsNullOrEmpty(PasswordEntry.Text);
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
