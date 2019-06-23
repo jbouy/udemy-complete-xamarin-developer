61edd556600ec13b726dacaaf197bf628d5957c4
James Bouy <jbouy@turner-industries.com>
2019-06-22 22:13:12 -0500

    s3: Hello World with Xamarin.Forms

---
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            var userName = NameEntry.Text;
+            var greeting = $"Hello {userName}!";
+
+            GreetingLabel.Text = greeting;
+        }
+    }
+}
